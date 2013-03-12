f3630e91037058ff90f2dcd3746879cbfa279bec
kchapl <kelvin.chappell@gmail.com>
2013-03-12 22:05:54 +0000

    init

---
+++ project/Build.scala
@@ -0,0 +1,21 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "book-log"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    "postgresql" % "postgresql" % "8.4-702.jdbc4",
+    jdbc,
+    anorm
+  )
+
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here      
+  )
+
+}
