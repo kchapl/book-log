9b792341536895eca5b12f6eeb57d41cb87f2efc
kchapl <kchapl@users.noreply.github.com>
2023-07-11 18:23:41 +0000

    Reformat with scalafmt 3.7.8
    Executed command: scalafmt --non-interactive

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -5,10 +5,9 @@ package test
 import play.api.test._
 import play.api.test.Helpers._
 
-/**
- * add your integration spec here.
- * An integration test will fire up a whole play application in a real (or headless) browser
- */
+/** add your integration spec here. An integration test will fire up a whole
+  * play application in a real (or headless) browser
+  */
 //class IntegrationSpec extends Specification {
 //
 //  "Application" should {
